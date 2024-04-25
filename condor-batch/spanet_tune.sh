35acbdf0e13a4c4b508c50e3f4c4945b92e4e7f3
Joxy97 <Joxy97@yahoo.com>
2024-04-25 19:40:05 +0200

    Fixed options

--- condor-batch/spanet_tune.sh
+++ condor-batch/spanet_tune.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 date
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 ulimit -u 127590
 python3 -m spanet.tune /eos/user/j/jmitic/hhh/options_files/hhh_v33_SVB.json --gpus 4
 date
